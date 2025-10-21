1e173ea3ffc8c8db2c741301786c192663e24025
Tim Roadley <timroadley@icloud.com>
2025-10-21 11:45:35 +1100

    Create Constants.swift

---
+++ Groceries/Groceries/System/Constants.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+//
+//  Constants.swift
+//
+//  Created by Tim Roadley on 21/10/2025.
+//
+
+enum Constants {
+    enum App {
+        static let bundleIdentifier = Bundle.main.bundleIdentifier ?? "com.roadley.Groceries"
+    }
+
+//    enum Logging {
+//        static let persistenceCategory = "Persistence"
+//    }
+//
+//    enum API {
+//        static let baseURL = URL(string: "https://api.example.com")!
+//    }
+}
+
