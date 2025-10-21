41cdbbdf9c412376e1717ed412f7737fa5792614
Tim Roadley <timroadley@icloud.com>
2025-10-21 11:45:38 +1100

    Update GroceriesApp.swift

--- Groceries/Groceries/GroceriesApp.swift
+++ Groceries/Groceries/GroceriesApp.swift
@@ -14,7 +14,7 @@ struct GroceriesApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BuildingView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
