3374b65462d4e63842d39607f06bec01a0ce96d0
Tim Roadley <timroadley@icloud.com>
2025-10-21 12:43:22 +1100

    Update Persistence.swift

--- Groceries/Groceries/Persistence.swift
+++ Groceries/Groceries/Persistence.swift
@@ -14,10 +14,18 @@ struct PersistenceController {
     static let preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
+        
+        // Insert some example items
+        for _ in 0..<5 {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
         }
+        
+        // Insert some example building
+        let newBuilding = Building(context: viewContext)
+        newBuilding.name = "Test Building"
+        newBuilding.buildingType = 0
+        
         do {
             try viewContext.save()
         } catch {
@@ -47,14 +55,6 @@ struct PersistenceController {
         container.loadPersistentStores(completionHandler: {
             (storeDescription, error) in
             if let error = error as NSError? {
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 #if DEBUG
                     fatalError("Unresolved error \(error), \(error.userInfo)")
                 #else
