32538211450a76d97c99f05b0c3ffc39355323c8
Tim Roadley <timroadley@icloud.com>
2025-10-21 11:43:32 +1100

    watch app

---
+++ Groceries/Groceries Wrist Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Groceries Wrist Watch App
+//
+//  Created by Tim Roadley on 21/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Groceries/Groceries Wrist Watch App/Groceries_WristApp.swift
@@ -0,0 +1,17 @@
+//
+//  Groceries_WristApp.swift
+//  Groceries Wrist Watch App
+//
+//  Created by Tim Roadley on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Groceries_Wrist_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
