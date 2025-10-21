76d8b14737d5e29a624476b7fb2a384c450cc797
Tim Roadley <timroadley@icloud.com>
2025-10-21 12:04:44 +1100

    Update Persistence.swift

--- Groceries/Groceries/Persistence.swift
+++ Groceries/Groceries/Persistence.swift
@@ -21,10 +21,16 @@ struct PersistenceController {
         do {
             try viewContext.save()
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            let error = error as NSError
+            #if DEBUG
+                fatalError(
+                    "Failed to save preview data: \(error), \(error.userInfo)"
+                )
+            #else
+                log.error(
+                    "Failed to save preview data: \(error), \(error.userInfo)"
+                )
+            #endif
         }
         return result
     }()
@@ -34,13 +40,13 @@ struct PersistenceController {
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "Groceries")
         if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+            container.persistentStoreDescriptions.first!.url = URL(
+                fileURLWithPath: "/dev/null"
+            )
         }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores(completionHandler: {
+            (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -49,7 +55,11 @@ struct PersistenceController {
                  * The store could not be migrated to the current model version.
                  Check the error message to determine what the actual problem was.
                  */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                #if DEBUG
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                #else
+                    log.error("Unresolved error \(error), \(error.userInfo)")
+                #endif
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
